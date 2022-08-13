783e902fbdd83b610f4e67035a6f4183dee13895
jefferson magallanes <jeffersonmaga97@gmail.com>
2022-08-13 16:33:03 -0300

    ee

--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -24,7 +24,7 @@ const ItemDetail = ({ id, marca, precio, img, descripcion, stock }) => {
                         <h3 className='nombre'>{marca}</h3>
                         <div className='textdescripcion'> <p className='descripcion'>{descripcion}</p></div>
                         <p className='precio'> Precio: <b>{precio} ars.</b></p>
-                        { !carrito.find(articulo => articulo.id === id)
+                        { !carrito.some(articulo => articulo.id === id)
                             ?<Link to='/carrito'> terminar compra</Link>  && <Contador stock={stock} initial={1} onAdd={handleOnAdd} />
                             
                             : <Link to='/carrito'> terminar compra</Link> 
